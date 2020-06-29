afc6410822647e9bbfd204d95e2b629a8cc536e6
nurik91boss@gmail.com <nurik91boss@gmail.com>
2020-06-29 23:50:25 +0600

    feat: add basic routes

--- app/config/router.php
+++ app/config/router.php
@@ -1,11 +1,55 @@
 <?php
-
+use Phalcon\Mvc\Router;
 $router = $di->getRouter();
-
 $router->setDefaults([
     'namespace' => 'Controller',
     'controller' => 'index',
     'action' => 'index',
 ]);
+
+$groups = [
+    '/profile' => [
+        'namespace' => 'Controller\\Profile',
+        'controller' => 'profile',
+    ],
+    '/products' => [
+        'namespace' => 'Controller\\Products',
+        'controller' => 'products',
+    ],
+    '/categories' => [
+        'namespace' => 'Controller\\Categories',
+        'controller' => 'categories',
+    ],
+];
+
+foreach ($groups as $prefix => $groupDefinition) {
+    $groupRouter = new Router\Group($groupDefinition);
+
+    $groupRouter->setPrefix($prefix);
+
+    $groupRouter->addGet('/', [
+        'action' => 'index',
+    ]);
+
+    $groupRouter->addGet('/{id:[0-9]+}', [
+        'action' => 'view',
+    ]);
+
+    $groupRouter->addGet('/{id:[0-9]+}/edit', [
+        'action' => 'edit',
+    ]);
+
+    $groupRouter->addGet('/{id:[0-9]+}/delete', [
+        'action' => 'delete',
+    ]);
+
+    $groupRouter->addPost('/save', [
+        'action' => 'save',
+    ]);
+
+    $router->mount($groupRouter);
+}
+
+
 
 $router->handle();
